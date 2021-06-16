2d0c752e3963ee7e178f8722f3bf6ae2cef125e1
David Lawrence <davidl@jlab.org>
2021-06-16 11:15:48 -0400

    Move the functionality of the writeMetaData.C script into run_EIC_production.C in a way that makes it more site independent.

--- extras/run_EIC_production.sh
+++ extras/run_EIC_production.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-export HOME=/sphenix/u/${LOGNAME}
 source /cvmfs/eic.opensciencegrid.org/ecce/gcc-8.3/opt/fun4all/core/bin/ecce_setup.sh -n $6
 
-logFile="${12}.out"
 d=`date +%Y/%m/%d`
 t=`date +%H:%M`
 
-echo ====== Your production details ======
-echo Production started: ${d} ${t}
-echo Production site: $9
-echo ECCE build: $6
-echo ECCE macros branch: production_$7
-echo ECCE macros hash: $8
-echo PWG: $7
-echo Generator: ${10} 
-echo Collision type: ${11}
-echo Input file: $2
-echo Output file: $3
-echo Output dir: $5 
-echo Number of events: $1
-echo Skip: $4
-echo =====================================
+# Print production details to screen and to metadata file simultaneously
+cat << EOF | tee ${5}/${3}.txt
+====== Your production details ======
+Production started: ${d} ${t}
+Production site: $9
+Production Host: ${HOST}
+ECCE build: $6
+ECCE macros branch: production_$7
+ECCE macros hash: $8
+PWG: $7
+Generator: ${10} 
+Collision type: ${11}
+Input file: $2
+Output file: $3
+Output dir: $5 
+Number of events: $1
+Skip: $4
+=====================================
+EOF
 
+# Run Fun4all. Send output to stdout but also capture to temporary local file
 echo running root.exe -q -b Fun4All_G4_EICDetector.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\)
-root.exe -q -b Fun4All_G4_EICDetector.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\)
+root.exe -q -b Fun4All_G4_EICDetector.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\) | tee tmp.out
+
+# Scan stdout of Fun4all for random number seeds and add to metadata file
 echo production script finished, writing metadata
-root.exe -q -b writeMetaData.C\(\"$3\",\"$5\",\"${logFile}\"\)
+grep -i seed tmp.out >> ${5}/${3}.txt
+rm tmp.out
+
 echo "script done"
