79ffef0073b78c7d323bbe615ab6856aedb9b049
Wenliang Wenliang <billlee@ifarm1802.jlab.org>
2021-07-15 10:10:50 -0400

    before merging

--- configurations.py
+++ configurations.py
@@ -14,15 +14,17 @@ macrosVersion = {
   "latest" : "9daf451", #from 2021/05/11
   "20210618" : "db6dd0c",
   "20210624" : "5f210c7",
-  "diff_tagg_physics_IP6_June_16_2021" : "9266a35" 
+  "diff_tagg_physics_IP6_June_16_2021" : "9266a35",
+  "Diff_Tagg_Sim_July_03_2021" : "8fa40c8",
+  "Diff_Tagg_Sim_July_03_2021_eA_110GeV_IP8" : "Diff_Tagg_Sim_July_03_2021_eA_110GeV_IP8"
 }
 
 #PWGs
 ecceWorkingGroup = ['General', 'DiffractiveAndTagging', 'Electroweak', 'ExclusiveReactions', 'HFandJets', 'Inclusive', 'SIDIS', 'Spectroscopy']
 
 #Generators
-ecceGenerator = ['particleGun', 'pythia6', 'pythia8', 'BeAGLE', 'Djangoh', 'MILOU3D', 'LAger', 'UVA', 'DPM', 'topeg', 'elSpectro']
+ecceGenerator = ['particleGun', 'pythia6', 'pythia8', 'BeAGLE', 'Djangoh', 'MILOU3D', 'LAger', 'UVA', 'DPM', 'topeg', 'elSpectro', 'DEMP']
 
 #Collision type
 #ecceCollision = ['singlePion', 'singleElectron', 'ep_5x100', 'ep_10x100', 'ep_18x100', 'ep_18x100lowq2', 'ep_18x100highq2']
-ecceCollision = ['singlePion', 'singleElectron', 'ep_5x100', 'ep_10x100', 'ep_18x100', 'ep_18x100lowq2', 'ep_18x100highq2', 'pionFF']
+ecceCollision = ['singlePion', 'singleElectron', 'ep_5x100', 'ep_10x100', 'ep_18x100', 'ep_18x100lowq2', 'ep_18x100highq2', 'pionFF', 'NeutronSS', 'u_channel_pi0', 'ePb_diffractive', 'SRC']
