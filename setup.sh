b94c8f9cc245ba8d4fa8ed9c3de67ce745a80fe1
Christoph Paus <paus@mit.edu>
2021-08-18 13:04:34 -0400

    Adding more testiong.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,17 @@ export PENV_CONDOR_REQ="( GLIDEIN_Site == \"MIT_CampusFactory\" && BOSCOGroup ==
 
 export PATH=${PATH}:${PENV_BASE}/bin:/cvmfs/eic.opensciencegrid.org/ecce/gcc-8.3/opt/fun4all/utils/bin
 export PYTHONPATH=${PYTHONPATH}:${PENV_BASE}/python
+
+if   [ "$1" == "T3" ]
+then
+  export PENV_CONDOR_REQ="( GLIDEIN_Site == \"MIT_CampusFactory\" && BOSCOGroup == \"bosco_tier3\" && ( regexp(\"T3B.*\",MACHINE) ) )"
+elif [ "$1" == "T2" ]
+then
+  export PENV_CONDOR_REQ="( GLIDEIN_Site == \"MIT_CampusFactory\" && BOSCOGroup == \"bosco_lns\" )"
+elif [ "$1" == "T23" ]
+then
+  export PENV_CONDOR_REQ="( GLIDEIN_Site == \"MIT_CampusFactory\" && ( BOSCOGroup == \"bosco_lns\" || BOSCOGroup == \"bosco_tier3\" ) )"
+elif [ "$1" == "EN" ]
+then
+  export PENV_CONDOR_REQ="( BOSCOCluster == \"eofe7.mit.edu\" && BOSCOGroup == \"paus\" )"
+fi  
