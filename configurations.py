ad2df6fb73b993f438440dfeda7ef0a5156e788d
Cameron T. Dean <ctdean@eic0111.rcf.bnl.gov>
2021-11-11 10:28:13 -0500

    Updated to prop.5.1

--- configurations.py
+++ configurations.py
@@ -40,6 +40,10 @@ macrosVersion = {
   "prop.5.0-production-pythia8" : "b4cfbdc",
   "prop.5.0-production-singlePion-0-20GeV" : "ef2da02",
   "prop.5.0-production-singleElectron-0-20GeV" : "ba98f0d",
+  "prop.5.1-production" : "40d17f7",
+  "prop.5.1-production-pythia8" : "35c63fb",
+  "prop.5.1-production-singlePion-0-20GeV" : "b399f7a",
+  "prop.5.1-production-singleElectron-0-20GeV" : "aa63c2a",
 #Diff and Tagg
 # r1
   "diff_tagg_physics_IP6_June_16_2021" : "9266a35",
